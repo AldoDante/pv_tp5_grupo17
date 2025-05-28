72221b50e66dbcc5c236c9bfa73f2f64a6bbb469
GonzaTaritolay <gonzalotaritolay94@gmail.com>
2025-05-28 20:54:28 -0300

    estilo de carta de alumno

--- pv_tp5_grupo17/src/pages/DetalleAlumno.jsx
+++ pv_tp5_grupo17/src/pages/DetalleAlumno.jsx
@@ -1,8 +1,11 @@
 import { useParams } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { useNavigate } from 'react-router-dom';
 
 export default function DetalleAlumno({ alumnos }) {
   const { id } = useParams();
-
+  const navigate = useNavigate();
   const alumno = alumnos.find(a => a.id === id);
 
   if (!alumno) return <p>Alumno no encontrado</p>;
@@ -10,13 +13,21 @@ export default function DetalleAlumno({ alumnos }) {
   return (
     <div>
       <h2>Detalles del Alumno</h2>
-      <p><strong>ID:</strong> {id}</p>
-      <p><strong>Nombre:</strong> {alumno.nombre}</p>
-      <p><strong>Apellido:</strong> {alumno.apellido}</p>
-      <p><strong>Curso:</strong> {alumno.curso}</p>
-      <p><strong>Email:</strong> {alumno.email}</p>
-      <p><strong>Domicilio:</strong> {alumno.domicilio}</p>
-      <p><strong>Teléfono:</strong> {alumno.telefono}</p>
+      <Card style={{ width: '18rem', margin: '20px auto' }}>
+        <Card.Img variant="top" src="/images/perfil.jpg" />
+        <Card.Body>
+          <Card.Title> Alumno: {alumno.nombre} {alumno.apellido}</Card.Title>
+          <div className="card-text">
+            <p><strong>ID:</strong> {id}</p>
+            <p><strong>Curso:</strong> {alumno.curso}</p>
+            <p><strong>Email:</strong> {alumno.email}</p>
+            <p><strong>Domicilio:</strong> {alumno.domicilio}</p>
+            <p><strong>Teléfono:</strong> {alumno.telefono}</p>
+          </div>
+
+          <Button variant="primary" onClick={() => navigate('/alumnos')}>Volver a la lista</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
