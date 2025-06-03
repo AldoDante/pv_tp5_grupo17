8c696f4299134ba12ce69455b789c651cf0c73c0
Jnh1212 <42804472@gmail.com>
2025-06-03 12:37:00 -0300

    Inicio

--- pv_tp5_grupo17/src/pages/Home.jsx
+++ pv_tp5_grupo17/src/pages/Home.jsx
@@ -5,9 +5,9 @@ export default function Home() {
     <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "80vh" }}>
       <Row>
         <Col>
-          <Card className="shadow-lg p-4 rounded-4 border-0" bg="light">
+          <Card className="shadow-lg p-3 rounded-4 border-0 card-bienvenida card-bienvenida-home">
             <Card.Body>
-              <h2 className="text-center text-dark mb-0">
+              <h2 className="bienvenida text-center text-dark mb-0 my-0">
                 Bienvenido al sistema de gestión de alumnos
               </h2>
             </Card.Body>
