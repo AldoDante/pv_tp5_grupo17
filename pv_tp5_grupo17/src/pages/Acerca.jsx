50fead491c1b49c8888129ca1ba667c9f45fe307
Jnh1212 <42804472@gmail.com>
2025-06-03 21:34:41 -0300

    Acerca de

--- pv_tp5_grupo17/src/pages/Acerca.jsx
+++ pv_tp5_grupo17/src/pages/Acerca.jsx
@@ -2,11 +2,11 @@ import { Container, Card } from "react-bootstrap";
 
 export default function Acerca() {
   return (
-    <Container className="mt-5 d-flex justify-content-center">
-      <Card className="p-4 shadow-lg rounded-4 border-0" style={{ maxWidth: "600px", width: "100%" }}>
+    <Container className="pagina-animada mt-5 d-flex justify-content-center">
+      <Card className="acerca-card p-4 shadow-lg rounded-4 border-0" style={{ maxWidth: "600px", width: "100%" }}>
         <Card.Body>
-          <h4 className="text-center mb-3">Acerca de la aplicación</h4>
-          <p className="text-center text-muted">
+          <h4 className="acerca-titulo text-center mb-3">Acerca de la aplicación</h4>
+          <p className="acerca-texto text-center text-muted">
             Creado por el grupo 17 - Programación Visual. <br />
             Esta app es un ejemplo de CRUD en React.
           </p>
